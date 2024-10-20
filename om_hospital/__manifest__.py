d9ce78c96f91249b0d69412a0380016b1b9608db
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-10-20 07:14:10 +0000

    first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Customizations',
+    # 'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Custom fields for Sales module',
+    "author":"Mohtashim",
+    "version":"17.0.1.3",
+    "license":"LGPL-3",
+    'depends': ['mail'],  # Make sure this module depends on the 'sale' module
+    'data': [
+        "security/ir.model.access.csv",
+        "views/patient_readonly_views.xml",
+        "views/menu.xml",
+        "views/patient_views.xml"
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ sales_customization/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sales Customizations',
+    # 'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Custom fields for Sales module',
+    "author":"Mohtashim",
+    "version":"17.0.1.3",
+    "license":"LGPL-3",
+    'depends': ['base'],  # Make sure this module depends on the 'sale' module
+    # 'data': [
+    #     'views/sale_order_view.xml',  # Reference to the view file
+    # ],
+    'installable': True,
+    'application': False,
+}
